cf51381d26768a75e2231cb3df4bb15f3b7238b3
Anton Yemelyanov <0404@i.ua>
2018-10-29 10:25:11 +0200

    Created: module,route,controller,layout,template

---
+++ app/code/YemelyanovAg/Lesson3/Controller/ShowPerson/Index.php
@@ -0,0 +1,15 @@
+<?php
+namespace YemelyanovAg\Lesson3\Controller\ShowPerson;
+use Magento\Framework\App\Action\Context;
+class Index extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @return \Magento\Framework\App\ResponseInterface|\Magento\Framework\Controller\ResultInterface|void
+     */
+    public function execute()
+    {
+
+        $this->_view->loadLayout();
+        $this->_view->renderLayout();
+    }
+}
