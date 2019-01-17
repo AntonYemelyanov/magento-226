09d198abb77d3947b5c5ee4a8585ad78bf888afe
Anton Yemelyanov <aeroenergetic@gmail.com>
2019-01-17 15:38:23 +0200

    Lesson 9 is done

---
+++ app/code/YemelyanovAg/CustomWidget/Block/Widget/HomepageBanner.php
@@ -0,0 +1,23 @@
+<?php
+//namespace YemelyanovAg\CustomWidget\Block\Widget;
+//use Magento\Framework\View\Element\Template;
+//use Magento\Widget\Block\BlockInterface;
+//
+//class HomepageBanner extends Template implements BlockInterface {
+//    protected $_template = "widget/homepage_banner.phtml";
+//}
+
+
+
+
+namespace YemelyanovAg\CustomWidget\Block\Widget;
+use Magento\Framework\View\Element\Template;
+use Magento\Widget\Block\BlockInterface;
+
+class HomepageBanner extends Template implements BlockInterface
+{
+    /**
+     * @var string
+     */
+    protected $_template = 'widget/homepage_banner.phtml';
+}
